0553e226b9c9b292618c3035a0cf2028b9182dd3
Ayush-Vishwakarma <ayush.vishwakarma@shorthills.ai>
2025-04-01 14:55:05 +0530

    added the mathFun file for demonstrating math module usage

---
+++ mathFun.py
@@ -0,0 +1,26 @@
+import math
+
+x=4
+y=2
+
+print('pow function usage:')
+print(math.pow(x,y))
+
+decimal=1.3456782
+print('usage of ciel function:')
+print(math.ceil(decimal))
+
+print('usage of exp function:')
+print(math.exp(2))
+
+print('factorial of 4:')
+print(math.factorial(4))
+
+# constants in maths
+
+print('the constants in maths :')
+
+print(math.pi)
+print(math.nan)
+print(math.inf)
+print(math.e)
