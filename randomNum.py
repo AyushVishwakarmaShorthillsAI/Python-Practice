8a9f9ce87c7bd8ba6bbf890279c1c7a754ce0bbd
Ayush-Vishwakarma <ayush.vishwakarma@shorthills.ai>
2025-04-01 14:40:16 +0530

    added extra random functions

--- randomNum.py
+++ randomNum.py
@@ -2,4 +2,12 @@ import random
 
 
 print('a random no. in a range b/w lower and higher value: ')
-print(random.randrange(1, 10));
+print(random.randrange(1, 10))
+
+arr=[1,3,4,5,9,10,56,33, 88, 23, 11]
+print('shuffles the array in a random order:')
+random.shuffle(arr)
+print(arr)
+
+print('a random number b/w 0 and 1:')
+print(random.random())
